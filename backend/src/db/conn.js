f6649f27aefd2e1978d2872bddb5df8cace8e91b
Joel Mathew V <joelmathewv0819@gmail.com>
2023-05-10 12:29:25 +0530

    database to atlas

--- backend/src/db/conn.js
+++ backend/src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/regis").then(() => {
+mongoose.connect("mongodb+srv://joelmathewv0819:pIy1n3gw0pJwEwgm@autoworkshop.eb7cuhr.mongodb.net/?retryWrites=true&w=majority/regis").then(() => {
     console.log("success")
 }).catch(error => handleError(error));
